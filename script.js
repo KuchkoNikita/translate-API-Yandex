f5e421d6bd84a1c928148ed35a4f6e8345beac29
Nikita Kuchko <kuchko.n.study@gmail.com>
2020-03-02 16:50:23 +0300

    add files, button and input

---
+++ script.js
@@ -0,0 +1,7 @@
+const input = document.querySelector('.input');
+const button = document.querySelector('.button');
+const translatedText = document.querySelector('.translated-text');
+
+button.addEventListener('click', () => {
+
+});
